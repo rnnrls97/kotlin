529c0c45e5a7f5a6cf5574e84fb0d601254ab370
makzs <erikmakiak@gmail.com>
2024-11-06 19:46:42 -0300

    fixed project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
-    namespace = "com.example.pageapp"
+    namespace = "com.codewithfk.expensetracker.android"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.pageapp"
+        applicationId = "com.codewithfk.expensetracker.android"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -59,14 +61,28 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.gridlayout)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.cardview)
+    implementation(libs.constraintlayout)
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
+    implementation(libs.dagger.hilt.andriod)
+    kapt(libs.dagger.hilt.compiler)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.compose.navigation)
+    implementation(libs.dagger.hilt.compose)
+    implementation("androidx.compose.foundation:foundation:1.7.0-beta07")
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
-rootProject.name = "pageApp"
+rootProject.name = "Expense Tracker Android"
 include(":app")
- 
